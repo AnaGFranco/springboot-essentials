12279bf54958de154be1a6ba7bdb55f92a1b57bf
AnaGFranco <anagsfranco@hotmail.com>
2020-05-28 23:51:03 -0300

    Lista de alunos

--- src/main/java/br/com/devdojo/model/Student.java
+++ src/main/java/br/com/devdojo/model/Student.java
@@ -1,12 +1,27 @@
 package br.com.devdojo.model;
 
+import static java.util.Arrays.asList;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
 
+	private int id;
 	private String name;
+	public static List<Student> studentList;
 	
 	
-	
+	public Student(int id, String name) {
+		this.setId(id);
+		this.name = name;
+	}
+
 	public Student() {
+	}
+	
+	private static void studantRepository() {
+		studentList = new ArrayList<>(asList(new Student("Ana"), new Student("Gabriela")));
 	}
 	
 	public Student(String name) {
@@ -18,6 +33,14 @@ public class Student {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 }
 
