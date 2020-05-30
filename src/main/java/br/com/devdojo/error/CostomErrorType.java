38a62bcb00169f4c67d957a553e9f119fe3d7265
AnaGFranco <anagsfranco@hotmail.com>
2020-05-30 00:46:03 -0300

    Criando classe CostomErrorType para retornar  error

---
+++ src/main/java/br/com/devdojo/error/CostomErrorType.java
@@ -0,0 +1,21 @@
+package br.com.devdojo.error;
+
+/**
+ * 
+ * @author Ana Gabriela Franco
+ */
+
+public class CostomErrorType {
+
+	private String errorMessage;
+
+	public CostomErrorType(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+	
+	
+}
