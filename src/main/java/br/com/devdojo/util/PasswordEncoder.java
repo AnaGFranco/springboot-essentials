b3010142be9311fc944c824acd0c1a3bf8ae7741
AnaGFranco <anagsfranco@hotmail.com>
2020-06-10 15:27:20 -0300

    Autenticação e autorização com Spring Data

--- src/main/java/br/com/devdojo/util/PasswordEncoder.java
+++ src/main/java/br/com/devdojo/util/PasswordEncoder.java
@@ -1,5 +1,10 @@
 package br.com.devdojo.util;
 
-public class PasswordEncoder {
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+public class PasswordEncoder {
+	public static void main(String[] args) {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		System.out.println(passwordEncoder.encode("ana"));
+	}
 }
